8a3b34c103e95e9dc3ae739a08f3a7cff403f26e
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-22 18:46:03 +0400

    Initial Commit

---
+++ MuscleMate/MuscleMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleMateApp.swift
+//  MuscleMate
+//
+//  Created by aplle on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MuscleMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
