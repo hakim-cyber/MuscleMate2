ecd5c3f95181be915ff9ac54a4c18e3fdcd93c91
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-26 23:05:30 +0400

    Madde it to show monday tuesday instead of day1,2

--- MuscleMate/ContentView.swift
+++ MuscleMate/ContentView.swift
@@ -47,7 +47,7 @@ struct ContentView: View {
                                         
                                         HStack{
                                             Spacer()
-                                            Text("Day \(daysOfWeek[index].id)")
+                                            Text("\(CheckWeekDay(day:daysOfWeek[index]))")
                                                 .foregroundColor(.black)
                                                 .font(.largeTitle)
                                                 .bold()
@@ -136,6 +136,28 @@ struct ContentView: View {
             daysOfWeek.remove(at: index)
             save()
         }
+    func CheckWeekDay(day:Day) ->String{
+        switch day.id{
+        case 1:
+            return "Monday"
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thursday"
+        case 5:
+            return "Friday"
+        case 6:
+            return "Saturday"
+        case 7:
+            return "Sunday"
+       
+            
+        default:
+            return "Day of Week"
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
