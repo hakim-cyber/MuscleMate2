1a664a72848bd0a0be77fe2eb6fb09ad8ee7ee38
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-29 00:10:59 +0400

    DayView model start

--- MuscleMate/DayView-ViewModel.swift
+++ MuscleMate/DayView-ViewModel.swift
@@ -6,8 +6,75 @@
 //
 
 import Foundation
+import SwiftUI
 
 
-class DayView_viewModel:ObservableObject{
+class DayView_viewModel:ObservableObject {
+    @Binding var day:Day
+    var change: (() -> Void)? = nil
     
+    @Published var days = [Day]()
+    
+    @Published var availibleMuscles = ["back",
+                                       "cardio",
+                                       "chest",
+                                       "lower arms",
+                                       "lower legs",
+                                       "neck",
+                                       "shoulders",
+                                       "upper arms",
+                                       "upper legs",
+                                       "waist"]
+    @Published var pickedMuscle = "back"
+    
+    @Published var showadding =  false
+    @Published var ShowStartWorkout = false
+    
+    init(day: Binding<Day>)  {
+        self._day = day
+    }
+    
+    func save(){
+        if let encoded = try? JSONEncoder().encode(days){
+            UserDefaults.standard.set(encoded, forKey: Day.saveKey)
+        }
+    }
+    
+    func newAvailible(){
+       var  usedMuscles = [String]()
+        for muscle in day.muscles{
+            usedMuscles.append(muscle.muscle.lowercased())
+        }
+        availibleMuscles = availibleMuscles.filter{!(usedMuscles.contains($0))}
+        if !availibleMuscles.isEmpty{
+            pickedMuscle = availibleMuscles[0]
+        }
+    }
+    
+    func remove(index:Int){
+        day.muscles.remove(at: index)
+        change?()
+    }
+    
+    
+    var estimatedTimeForWorkout:Int{
+        var setsCount = 0
+        for muscle in day.muscles {
+            for excercise in muscle.exercises{
+                setsCount += excercise.sets!
+            }
+        }
+        
+        let estimatedTime = setsCount * 120 + (setsCount - 1) * 120
+        if estimatedTime > 0{
+            return estimatedTime
+        }else{
+            return 0
+        }
+    }
+    
+    var estimatedCalories:String{
+        var estimatedInHours = Double(estimatedTimeForWorkout) / 3600
+        return String(format: "%.1f", estimatedInHours * 300)
+    }
 }
